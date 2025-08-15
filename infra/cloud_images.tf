b12c2fad499d28728a3d7d7546fad0172c0e4bc7
Erik Badger <erikbadger777@gmail.com>
2025-08-15 10:23:18 -0500

    feat: upgraded to debian 13

--- infra/cloud_images.tf
+++ infra/cloud_images.tf
@@ -2,7 +2,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "import"
   datastore_id = "local"
   node_name    = "node1"
-  url          = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2"
+  url          = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
   # need to rename the file to *.qcow2 to indicate the actual file format for import
   file_name = "debian-12-genericcloud-amd64.qcow2"
   overwrite = true
