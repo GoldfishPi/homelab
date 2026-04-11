61e0ea9d9c838bf840708ff79cb0da2e25a7d7b5
Erik Badger <erikbadger777@gmail.com>
2026-04-11 14:28:42 -0500

    feat: added services

---
+++ services/dev/immich_service/immich.tf
@@ -0,0 +1,155 @@
+terraform {
+  required_providers {
+    openwrt = {
+      source = "joneshf/openwrt"
+      version = "0.0.20"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+resource "kubernetes_namespace_v1" "immich" {
+  metadata {
+    name = "immich"
+  }
+}
+
+resource "kubernetes_persistent_volume_v1" "immich_data" {
+  metadata {
+    name = "nfs-pv"
+  }
+  spec {
+    capacity = {
+      storage = "250Gi"
+    }
+    access_modes = ["ReadWriteMany"]
+    storage_class_name = "nfs"
+    persistent_volume_reclaim_policy = "Retain"
+    persistent_volume_source {
+      nfs {
+        server = "truenas.lan"
+        path = "/mnt/pool/k8s/immich"
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "immich_data" {
+  metadata {
+    name = "immich-library-pvc"
+    namespace = kubernetes_namespace_v1.immich.metadata[0].name
+  }
+  spec {
+    storage_class_name = "nfs"
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "250Gi"
+      }
+    }
+  }
+  wait_until_bound = false
+}
+
+data "http" "cnpg_manifest" {
+  url = "https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.29/releases/cnpg-1.29.0.yaml"
+}
+
+data "kubectl_file_documents" "cnpg_docs" {
+  content = data.http.cnpg_manifest.response_body
+}
+
+resource "kubectl_manifest" "cnpg" {
+  for_each = data.kubectl_file_documents.cnpg_docs.manifests
+
+  yaml_body         = each.value
+  server_side_apply = true
+  force_conflicts   = true
+}
+
+
+resource "kubectl_manifest" "cnpg_cluster" {
+  yaml_body = file("${path.module}/postgres.yaml")
+  depends_on = [kubectl_manifest.cnpg]
+}
+
+resource "helm_release" "immich" {
+  namespace = kubernetes_namespace_v1.immich.metadata[0].name
+  name = "immich"
+
+  repository = "https://immich-app.github.io/immich-charts"
+  chart      = "immich"
+
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+  depends_on = [kubectl_manifest.cnpg_cluster, kubernetes_persistent_volume_v1.immich_data]
+}
+
+resource "openwrt_dhcp_domain" "immich" {
+  id   = "immich"
+  ip   = var.host
+  name = "immich.${var.namespace}"
+}
+
+resource "kubectl_manifest" "ingress" {
+  yaml_body = yamlencode({
+    apiVersion = "networking.k8s.io/v1"
+    kind = "Ingress"
+    metadata = {
+      # For cert manager
+      # annotations:                                # Add an annotation indicating the certificate issuer to use.
+      #   cert-manager.io/cluster-issuer: ca-issuer # Cluster-wide ClusterIssuer
+      name = "immichingress",
+      namespace = kubernetes_namespace_v1.immich.metadata[0].name
+    }
+    spec = {
+      ingressClassName = "traefik"
+      # tls:                                       # Placing a host in the TLS config will determine what ends up in the cert's subjectAltNames
+      # - hosts:
+      #   - immich.192-168-0-37.nip.io
+      #   secretName: immich-cert                    # cert-manager will store the new certificate in this secret.
+      rules = [
+        {
+          host = openwrt_dhcp_domain.immich.name,
+          http = {
+            paths = [
+              {
+                path = "/"
+                pathType = "Prefix"
+                backend = {
+                  service = {
+                    name: "immich-server"
+                    port = {
+                      number = 2283
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  })
+  depends_on=[helm_release.immich]
+}
+
+output "url" {
+  value = openwrt_dhcp_domain.immich.name
+}
---
+++ services/dev/services.tf
@@ -0,0 +1,125 @@
+
+terraform {
+  required_providers {
+    ansible = {
+      version = "~> 1.4.0"
+      source  = "ansible/ansible"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    dns = {
+      source = "hashicorp/dns"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+    openwrt = {
+      source = "joneshf/openwrt"
+      version = "0.0.20"
+    }
+  }
+}
+
+variable "openwrt_password" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+provider "dns" {}
+
+
+data "terraform_remote_state" "provisioning" {
+  backend = "local"
+   
+  config = {
+    path = "${path.module}/../../provisioning/dev/terraform.tfstate"
+  }
+}
+
+data "dns_a_record_set" "server" {
+  host = data.terraform_remote_state.provisioning.outputs.k3s_server
+}
+
+resource "ansible_playbook" "server_kubeconfig" {
+  playbook   = "${path.module}/playbooks/read_server_kubeconfig.yaml"
+  name       = data.terraform_remote_state.provisioning.outputs.k3s_server
+  replayable = false
+
+  extra_vars = {
+    ansible_user                 = "erik"
+    ansible_ssh_private_key_file = "~/.ssh/id_rsa"
+  }
+
+}
+
+locals {
+  host = data.dns_a_record_set.server.addrs[0]
+  ca_certificate = base64decode(regex(
+    "TF_K8S_CLUSTER_CA_CERTIFICATE=(.+)\"",
+    ansible_playbook.server_kubeconfig.ansible_playbook_stdout
+  )[0])
+  client_certificate = base64decode(regex(
+    "TF_K8S_CLIENT_CERTIFICATE=(.+)\"",
+    ansible_playbook.server_kubeconfig.ansible_playbook_stdout
+  )[0])
+  client_key= base64decode(regex(
+    "TF_K8S_CLIENT_KEY=(.+)\"",
+    ansible_playbook.server_kubeconfig.ansible_playbook_stdout
+  )[0])
+}
+
+provider "kubernetes" {
+  host = "https://${local.host}:6443"
+  client_key = local.client_key
+  cluster_ca_certificate = local.ca_certificate
+  client_certificate = local.client_certificate
+}
+
+provider "helm" {
+  kubernetes = {
+    host = "https://${local.host}:6443"
+    client_key = local.client_key
+    client_certificate = local.client_certificate
+    cluster_ca_certificate = local.ca_certificate
+  }
+}
+
+provider "kubectl" {
+  host = "https://${local.host}:6443"
+  client_key = local.client_key
+  cluster_ca_certificate = local.ca_certificate
+  client_certificate = local.client_certificate
+  load_config_file       = false
+}
+
+resource "kubernetes_namespace_v1" "hello" {
+  metadata {
+    name = "test"
+  }
+}
+
+provider "openwrt" {
+  password = var.openwrt_password
+}
+
+module "immich" {
+  source = "./immich_service/"
+  host = local.host
+  namespace = var.namespace
+  providers = {
+    kubernetes = kubernetes
+    kubectl = kubectl
+    helm = helm
+    openwrt = openwrt
+  }
+}
+
+output "immich_url" {
+  value = "http://${module.immich.url}"
+}
